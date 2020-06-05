d11b78106a095f411c4a36d2e78cf40af5cd17d0
Saurav Kumar <62166399+sksharma-123@users.noreply.github.com>
2020-06-05 15:18:44 -0700

    Modified

--- editor.js
+++ editor.js
@@ -25,28 +25,21 @@ defaultbtn.addEventListener('click', () => {
     document.getElementById('editor').style.display = 'initial';
     document.getElementById('liveEditor').style.display = 'none';
     document.getElementById('run-span').innerHTML = 'Run';
+    var html = first.textContent;
+    iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
 })
 
 
 livebtn.addEventListener('click', () => {
     document.getElementById('editor').style.display = 'none'
     document.getElementById('liveEditor').style.display = 'initial'
-    document.getElementById('run-span').innerHTML = 'Live'
+    document.getElementById('run-span').innerHTML = 'Live';
+    var html = third.textContent;
+    iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
     third.addEventListener('keyup', () => {
         var html = third.textContent;
         iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
     })
 })
 
-// first.addEventListener('keyup', () => {
-//     var html = first.textContent;
-//     iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
-// })
-
-
-// first.addEventListener("paste", function (e) {
-//     e.preventDefault();
-//     var text = e.clipboardData.getData("text/plain");
-//     document.execCommand("insertText", false, text);
-// });
 
